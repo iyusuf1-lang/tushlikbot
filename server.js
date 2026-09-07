44ec92fc592210a271e98f3866b7a39709693dae
iyusuf1-lang <i_yusuf1@icloud.com>
2026-09-07 12:09:18 +0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const { Telegraf, Markup } = require('telegraf');
 const store = require('./store');
 
-const BOT_TOKEN = process.env.BOT_TOKEN;
+const BOT_TOKEN = process.env.8976669218:AAGbKlgtU3Eg4ynV_qGXs3t9R4XMjEocujY;
 const WEBAPP_URL = process.env.WEBAPP_URL;
 const PORT = process.env.PORT || 3000;
 const ADMIN_IDS = (process.env.ADMIN_IDS || '')
